8aa917123f7db1aa7e41b8f99ab3136e88e9105b
sungho-park <sungho-park@lotte.net>
2022-03-24 10:57:58 +0900

    Init

---
+++ train.py
@@ -0,0 +1,40 @@
+from datasets import load_dataset
+
+imdb = load_dataset("imdb")
+
+from transformers import (
+    AutoTokenizer, DataCollatorWithPadding, 
+    AutoModelForSequenceClassification, 
+    TrainingArguments, Trainer
+)
+
+model_name = 'cross-encoder/ms-marco-MiniLM-L-12-v2'
+mdoel_name = 'bert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=5,
+    weight_decay=0.01,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
